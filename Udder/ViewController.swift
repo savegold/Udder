b398d3d402629cc00ec1ad51b85af6946838bb56
Simone Salvoro <ssalvoro.dev@gmail.com>
2020-10-17 15:33:57 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Simone Salvoro on 17/10/2020.
+//  Copyright © 2020 Simone Salvoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
